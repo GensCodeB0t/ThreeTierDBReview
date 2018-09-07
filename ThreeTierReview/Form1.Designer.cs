ff8e0666fb216a444ab89f826751e7e6d603d9ca
GensCodeB0t <gensy83@gmail.com>
2018-09-07 12:15:22 -0500

    Add project files.

---
+++ ThreeTierReview/Form1.Designer.cs
@@ -0,0 +1,172 @@
+﻿namespace ThreeTierReview
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSubmit = new System.Windows.Forms.Button();
+            this.tbFirstName = new System.Windows.Forms.TextBox();
+            this.tbLastName = new System.Windows.Forms.TextBox();
+            this.tbEmployeeId = new System.Windows.Forms.TextBox();
+            this.lbFirstName = new System.Windows.Forms.Label();
+            this.lbLastName = new System.Windows.Forms.Label();
+            this.lbEmployeeId = new System.Windows.Forms.Label();
+            this.lbDepartment = new System.Windows.Forms.Label();
+            this.tbDepartment = new System.Windows.Forms.TextBox();
+            this.lbSalary = new System.Windows.Forms.Label();
+            this.tbSalary = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.Location = new System.Drawing.Point(79, 327);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(97, 23);
+            this.btnSubmit.TabIndex = 0;
+            this.btnSubmit.Text = "Submit";
+            this.btnSubmit.UseVisualStyleBackColor = true;
+            this.btnSubmit.Click += new System.EventHandler(this.btnSubmit_Click);
+            // 
+            // tbFirstName
+            // 
+            this.tbFirstName.Location = new System.Drawing.Point(76, 63);
+            this.tbFirstName.Name = "tbFirstName";
+            this.tbFirstName.Size = new System.Drawing.Size(100, 20);
+            this.tbFirstName.TabIndex = 1;
+            // 
+            // tbLastName
+            // 
+            this.tbLastName.Location = new System.Drawing.Point(76, 115);
+            this.tbLastName.Name = "tbLastName";
+            this.tbLastName.Size = new System.Drawing.Size(100, 20);
+            this.tbLastName.TabIndex = 2;
+            // 
+            // tbEmployeeId
+            // 
+            this.tbEmployeeId.Location = new System.Drawing.Point(76, 167);
+            this.tbEmployeeId.Name = "tbEmployeeId";
+            this.tbEmployeeId.Size = new System.Drawing.Size(100, 20);
+            this.tbEmployeeId.TabIndex = 3;
+            // 
+            // lbFirstName
+            // 
+            this.lbFirstName.AutoSize = true;
+            this.lbFirstName.Location = new System.Drawing.Point(76, 42);
+            this.lbFirstName.Name = "lbFirstName";
+            this.lbFirstName.Size = new System.Drawing.Size(60, 13);
+            this.lbFirstName.TabIndex = 4;
+            this.lbFirstName.Text = "First Name:";
+            // 
+            // lbLastName
+            // 
+            this.lbLastName.AutoSize = true;
+            this.lbLastName.Location = new System.Drawing.Point(76, 96);
+            this.lbLastName.Name = "lbLastName";
+            this.lbLastName.Size = new System.Drawing.Size(61, 13);
+            this.lbLastName.TabIndex = 5;
+            this.lbLastName.Text = "Last Name:";
+            // 
+            // lbEmployeeId
+            // 
+            this.lbEmployeeId.AutoSize = true;
+            this.lbEmployeeId.Location = new System.Drawing.Point(76, 148);
+            this.lbEmployeeId.Name = "lbEmployeeId";
+            this.lbEmployeeId.Size = new System.Drawing.Size(70, 13);
+            this.lbEmployeeId.TabIndex = 6;
+            this.lbEmployeeId.Text = "Employee ID:";
+            // 
+            // lbDepartment
+            // 
+            this.lbDepartment.AutoSize = true;
+            this.lbDepartment.Location = new System.Drawing.Point(76, 202);
+            this.lbDepartment.Name = "lbDepartment";
+            this.lbDepartment.Size = new System.Drawing.Size(62, 13);
+            this.lbDepartment.TabIndex = 7;
+            this.lbDepartment.Text = "Department";
+            // 
+            // tbDepartment
+            // 
+            this.tbDepartment.Location = new System.Drawing.Point(76, 219);
+            this.tbDepartment.Name = "tbDepartment";
+            this.tbDepartment.Size = new System.Drawing.Size(100, 20);
+            this.tbDepartment.TabIndex = 8;
+            // 
+            // lbSalary
+            // 
+            this.lbSalary.AutoSize = true;
+            this.lbSalary.Location = new System.Drawing.Point(76, 253);
+            this.lbSalary.Name = "lbSalary";
+            this.lbSalary.Size = new System.Drawing.Size(36, 13);
+            this.lbSalary.TabIndex = 9;
+            this.lbSalary.Text = "Salary";
+            // 
+            // tbSalary
+            // 
+            this.tbSalary.Location = new System.Drawing.Point(76, 270);
+            this.tbSalary.Name = "tbSalary";
+            this.tbSalary.Size = new System.Drawing.Size(100, 20);
+            this.tbSalary.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(268, 386);
+            this.Controls.Add(this.tbSalary);
+            this.Controls.Add(this.lbSalary);
+            this.Controls.Add(this.tbDepartment);
+            this.Controls.Add(this.lbDepartment);
+            this.Controls.Add(this.lbEmployeeId);
+            this.Controls.Add(this.lbLastName);
+            this.Controls.Add(this.lbFirstName);
+            this.Controls.Add(this.tbEmployeeId);
+            this.Controls.Add(this.tbLastName);
+            this.Controls.Add(this.tbFirstName);
+            this.Controls.Add(this.btnSubmit);
+            this.Name = "Form1";
+            this.Text = "New Employee";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSubmit;
+        private System.Windows.Forms.TextBox tbFirstName;
+        private System.Windows.Forms.TextBox tbLastName;
+        private System.Windows.Forms.TextBox tbEmployeeId;
+        private System.Windows.Forms.Label lbFirstName;
+        private System.Windows.Forms.Label lbLastName;
+        private System.Windows.Forms.Label lbEmployeeId;
+        private System.Windows.Forms.Label lbDepartment;
+        private System.Windows.Forms.TextBox tbDepartment;
+        private System.Windows.Forms.Label lbSalary;
+        private System.Windows.Forms.TextBox tbSalary;
+    }
+}
+
---
+++ ThreeTierReview/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.VisualStyles;
+
+namespace ThreeTierReview
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSubmit_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                btnSubmit.Enabled = false;
+                String fileEntry =
+                    $"{tbFirstName.Text} {tbLastName.Text} {tbEmployeeId.Text} {tbDepartment.Text} {tbSalary.Text}; ";
+                File.AppendAllText(@"C:\Users\Ryan\Desktop\Employee.txt", fileEntry.ToString() + "\r\n");
+
+                using (SqlConnection connection = new SqlConnection(ConfigurationManager.ConnectionStrings["EmployeeDB"].ConnectionString))
+                {
+                    String query =
+                        "INSERT INTO Employee(FirstName, LastName, EmployeeId, Department, Salary) VALUES(@param1, @param2, @param3, @param4, @param5)";
+                    SqlCommand cmd = new SqlCommand(query, connection);
+                    cmd.Parameters.Add("@param1", SqlDbType.Char).Value = tbFirstName.Text;
+                    cmd.Parameters.Add("@param2", SqlDbType.Char).Value = tbLastName.Text;
+                    cmd.Parameters.Add("@param3", SqlDbType.Char).Value = tbEmployeeId.Text;
+                    cmd.Parameters.Add("@param4", SqlDbType.Char).Value = tbDepartment.Text;
+                    cmd.Parameters.Add("@param5", SqlDbType.Money).Value = decimal.Parse(tbSalary.Text, NumberStyles.Currency);
+                    connection.Open();
+                    cmd.ExecuteNonQuery();
+                }
+
+                btnSubmit.Enabled = true;
+
+                foreach (var control in Enumerable.OfType<TextBox>(Controls))
+                    control.Text = String.Empty;
+            }
+            catch (Exception error)
+            {
+                if (error is SqlException)
+                    MessageBox.Show("An error occured while trying to update the database:\r\n" + error.Message);
+                else if (error is FileNotFoundException)
+                    MessageBox.Show("The file that you are trying to access is unaccessable:\r\n" + error.Message);
+                else if (error is FileNotFoundException)
+                    MessageBox.Show("The file that you are trying to access could not be found:\r\n" + error.Message);
+                else
+                    MessageBox.Show("An error has occured:\r\n" + error.Message);
+                throw;
+            }                            
+
+        }
+    }
+}
